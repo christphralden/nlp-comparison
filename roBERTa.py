2c271a80febe2b303195b66f95ebe7f8037ceac5
christopher-alden <christopheralden04@gmail.com>
2024-06-07 15:10:56 +0700

    roberta trained with dataset

--- roBERTa.py
+++ roBERTa.py
@@ -1,59 +1,84 @@
-from transformers import AutoTokenizer as at
-from transformers import AutoModelForSequenceClassification as amsc
-from scipy.special import softmax
 import pandas as pd
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer, DataCollatorWithPadding
+from sklearn.model_selection import train_test_split
+from datasets import Dataset
 import numpy as np
+from scipy.special import softmax
 import matplotlib.pyplot as plt
-import nltk
 from tqdm import tqdm
 
-MODEL_TYPE = f"cardiffnlp/twitter-roberta-base-sentiment"
-TOKENIZER = at.from_pretrained(MODEL_TYPE)
-MODEL = amsc.from_pretrained(MODEL_TYPE)
-
-#Id,ProductId,UserId,ProfileName,HelpfulnessNumerator,HelpfulnessDenominator,Score,Time,Summary,Text
+TRAIN = False # Toggle
 
 df = pd.read_csv('./Reviews.csv')
 df = df.head(10000)
-# df.set_index('Id', inplace=True)
-
-def polarity_scores_roberta(e):
-    encoded_text = TOKENIZER(e, return_tensors='pt')
-    output = MODEL(**encoded_text)
-    scores = output[0][0].detach().numpy()
-    scores = softmax(scores)
-    scores_dict = {
-        'roberta_neg': scores[0],
-        'roberta_neu': scores[1],
-        'roberta_pos': scores[2],
-        'text':e
-    }
-
-    return scores_dict
-
-# EVALUATE
-# res = {}
-# df_len = len(df)
-
-# for i, row in tqdm(df.iterrows(), total=df_len):
-#     try:
-#         text = row['Text']
-#         myId = row['Id']
-#         roberta_result = polarity_scores_roberta(text)
-#         res[myId] = roberta_result
-#     except RuntimeError:
-#         print(f'Broke at {myId}')
-
-# res_df = pd.DataFrame(res).T
-# res_df.to_csv('RoBERTa_Result.csv')
 
 
-# PROCESS
-res_df = pd.read_csv('./RoBERTa_Result.csv') # Id nya harus tambahin manual gw males
+def map_score_to_sentiment(score):
+    if score < 3:
+        return 0  # Negative
+    elif score == 3:
+        return 1  # Neutral
+    else:
+        return 2  # Positive
+
+
+df['Sentiment'] = df['Score'].apply(map_score_to_sentiment)
+
+train_texts, test_texts, train_labels, test_labels = train_test_split(
+    df['Text'], df['Sentiment'], test_size=0.2, random_state=42)
+
+train_df = pd.DataFrame({'text': train_texts, 'label': train_labels})
+test_df = pd.DataFrame({'text': test_texts, 'label': test_labels})
+train_dataset = Dataset.from_pandas(train_df)
+test_dataset = Dataset.from_pandas(test_df)
+
+MODEL_TYPE = "cardiffnlp/twitter-roberta-base-sentiment"
+tokenizer = AutoTokenizer.from_pretrained(MODEL_TYPE)
+model = AutoModelForSequenceClassification.from_pretrained(
+    MODEL_TYPE, num_labels=3)
+
+
+def tokenize_function(examples):
+    return tokenizer(examples['text'], truncation=True, padding='max_length', max_length=128)
+
+
+train_dataset = train_dataset.map(tokenize_function, batched=True)
+test_dataset = test_dataset.map(tokenize_function, batched=True)
+
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+training_args = TrainingArguments(
+    output_dir='./results',
+    num_train_epochs=3,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=64,
+    warmup_steps=500,
+    weight_decay=0.01,
+    logging_dir='./logs',
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=test_dataset,
+    data_collator=data_collator
+)
+
+if(TRAIN):
+    trainer.train()
+
+try:
+    predictions = trainer.predict(test_dataset)
+    preds = np.argmax(predictions.predictions, axis=1)
+except:
+    print("fuck")
+
+test_df['predicted'] = preds
 
 predicted_negative = []
 predicted_neutral = []
-predicted_postivie = []
+predicted_positive = []
 
 false_negative = []
 false_neutral = []
@@ -63,50 +88,44 @@ true_negative = []
 true_neutral = []
 true_positive = []
 
-for idx, result in res_df.iterrows():
-    score = df.loc[df['Id'] == idx+1, 'Score'].iloc[0]
-    #we will assume that 3 is neutral
-    
+for idx in range(len(test_df)):
+    score = test_df['label'].iloc[idx]
+    result = {
+        'roberta_neg': predictions.predictions[idx][0],
+        'roberta_neu': predictions.predictions[idx][1],
+        'roberta_pos': predictions.predictions[idx][2]
+    }
 
-    if score < 3: # case actually negative
+    if score == 0:  # fr negative
         true_negative.append(result)
-    elif score == 3: # case actually neutral
+    elif score == 1:  # fr neutral
         true_neutral.append(result)
-    elif score > 3 :
+    elif score == 2:  # fr positive
         true_positive.append(result)
 
-
-    if result['roberta_neg'] > result['roberta_pos'] and result['roberta_neg'] > result['roberta_neu']: # case where negative is dominant
+    if result['roberta_neg'] > result['roberta_pos'] and result['roberta_neg'] > result['roberta_neu']:  # predicted negative
         predicted_negative.append(result)
-        if score > 3: # false negatives
+        if score > 0:  # false negatives
             false_negative.append(result)
-    elif result['roberta_neu'] > result['roberta_neg'] and result['roberta_neu'] > result['roberta_pos']:
+    elif result['roberta_neu'] > result['roberta_neg'] and result['roberta_neu'] > result['roberta_pos']:  # predicted neutral
         predicted_neutral.append(result)
-        if score != 3:
+        if score != 1:
             false_neutral.append(result)
-    elif result['roberta_pos'] > result['roberta_neg'] and result['roberta_pos'] > result['roberta_neu']:
-        predicted_postivie.append(result)
-        if score < 3:
+    elif result['roberta_pos'] > result['roberta_neg'] and result['roberta_pos'] > result['roberta_neu']:  # predicted positive
+        predicted_positive.append(result)
+        if score < 2:
             false_positive.append(result)
-        
 
-def delta(true_values, pred_values):
-    true_set = set(true_values)
-    fake_predictions = []
-    for predicted in pred_values:
-        if predicted not in true_set:
-            fake_predictions.append(predicted)
-    return fake_predictions
 
 def plot_all_comparisons(true_negatives, predicted_negatives, false_negatives, true_positives, predicted_positives, false_positives, true_neutrals, predicted_neutrals, false_neutrals):
-    fig, axes = plt.subplots(1, 3, figsize=(15, 5))  # 1 row, 3 columns of subplots
+    fig, axes = plt.subplots(1, 3, figsize=(15, 5))
 
     data = [
         (true_negatives, predicted_negatives, false_negatives, 'Negatives'),
         (true_positives, predicted_positives, false_positives, 'Positives'),
-        (true_neutrals, predicted_neutrals, false_neutrals,'Neutrals')
+        (true_neutrals, predicted_neutrals, false_neutrals, 'Neutrals')
     ]
-    
+
     colors = ['blue', 'green', 'red']
 
     for ax, (true_data, predicted_data, false_prediction, title) in zip(axes, data):
@@ -122,4 +141,4 @@ def plot_all_comparisons(true_negatives, predicted_negatives, false_negatives, t
     plt.show()
 
 
-plot_all_comparisons(true_negative, predicted_negative, false_negative, true_positive, predicted_postivie, false_positive, true_neutral, predicted_neutral, false_neutral)
+plot_all_comparisons(true_negative, predicted_negative, false_negative, true_positive, predicted_positive, false_positive, true_neutral, predicted_neutral, false_neutral)
