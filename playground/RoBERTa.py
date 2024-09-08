5e1646bc9fe3b51fcb307c4d7ff5bfec29991495
christopher-alden <christopheralden04@gmail.com>
2024-09-08 17:05:52 +0700

    post revision

--- playground/RoBERTa.py
+++
@@ -1,8 +0,0 @@
-from transformers import AutoModelForSequenceClassification
-
-class RoBERTa:
-    def __init__(self, model_type='cardiffnlp/twitter-roberta-base-sentiment', num_labels=3):
-        self.model = AutoModelForSequenceClassification.from_pretrained(model_type, num_labels=num_labels)
-    
-    def get_model(self):
-        return self.model
